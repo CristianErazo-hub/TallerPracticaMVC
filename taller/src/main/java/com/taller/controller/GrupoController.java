ba94e099bf9ad33d7fc5a266254b292711bb8197
CristianErazo-hub <cristianerazo420@gmail.com>
2026-04-24 22:10:00 -0500

    GrupoService y GrupoController

--- taller/src/main/java/com/taller/controller/GrupoController.java
+++ taller/src/main/java/com/taller/controller/GrupoController.java
@@ -1,5 +1,24 @@
 package com.taller.controller;
 
+import com.taller.modelo.Grupo;
+import com.taller.service.GrupoService;
+
+import java.util.List;
+
 public class GrupoController {
 
+    private GrupoService service;
+
+    public GrupoController(GrupoService service) {
+        this.service = service;
+    }
+
+    public void crear(int idMateria, int idDocente, String aula, String horario) {
+        Grupo grupo = new Grupo(0, idMateria, idDocente, aula, horario);
+        service.crearGrupo(grupo);
+    }
+
+    public List<Grupo> listar() {
+        return service.listar();
+    }
 }
--- taller/src/main/java/com/taller/service/GrupoService.java
+++ taller/src/main/java/com/taller/service/GrupoService.java
@@ -1,5 +1,23 @@
 package com.taller.service;
 
+import com.taller.dao.GrupoDAO;
+import com.taller.modelo.Grupo;
+
+import java.util.List;
+
 public class GrupoService {
 
+    private GrupoDAO grupoDAO;
+
+    public GrupoService(GrupoDAO grupoDAO) {
+        this.grupoDAO = grupoDAO;
+    }
+
+    public void crearGrupo(Grupo grupo) {
+        grupoDAO.crear(grupo);
+    }
+
+    public List<Grupo> listar() {
+        return grupoDAO.listar();
+    }
 }
