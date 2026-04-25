498341c74a8d888365b538539b486c9efecaa783
CristianErazo-hub <cristianerazo420@gmail.com>
2026-04-24 22:04:03 -0500

    Agregando modelo Materia

--- taller/src/main/java/com/taller/modelo/Materia.java
+++ taller/src/main/java/com/taller/modelo/Materia.java
@@ -2,4 +2,41 @@ package com.taller.modelo;
 
 public class Materia {
 
+    private int id;
+    private String nombreMateria;
+    private int creditos;
+
+    public Materia() {
+    }
+
+    public Materia(int id, String nombreMateria, int creditos) {
+
+        this.id = id;
+        this.nombreMateria = nombreMateria;
+        this.creditos = creditos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void SetCreditos(int creditos) {
+        this.creditos = creditos;
+    }
 }
